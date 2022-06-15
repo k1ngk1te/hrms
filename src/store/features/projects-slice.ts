360437e1eca795dd3514013bd99b83ac001fa145
kingkite <kitewebdevelopers@gmail.com>
2022-06-15 23:40:15 +0100

    Changed the tagTypes for project slice

--- src/store/features/projects-slice.ts
+++ src/store/features/projects-slice.ts
@@ -8,213 +8,6 @@ import {
 	PROJECT_FILE_URL,
 	PROJECT_FILES_URL,
 	TASKS_URL,
-	TASK_URL
-} from "../../config";
-import {
-	ProjectListType,
-	PaginationType,
-	ProjectType,
-	ProjectCreateType,
-	ProjectFileCreateType,
-	ProjectFileType,
-	TaskCreateType,
-	TaskListType,
-	TaskType
-} from "../../types";
-
-export interface TaskPaginationType extends PaginationType {
-	project_id: string;
-}
-
-const generateProjectFile = (data: ProjectFileCreateType) => {
-	const form = new FormData()
-	form.append("name",data.name)
-	form.append("file",data.file)
-	return form;
-}
-
-const projectsApi = baseApi.injectEndpoints({
-	endpoints: (build) => ({
-		getProjects: build.query<ProjectListType, PaginationType>({
-			query: ({ limit, offset, search }) => ({
-				url: `${PROJECTS_URL}?offset=${search ? 0 : offset}&limit=${
-					search ? 0 : limit
-				}&search=${search || ""}`,
-				method: "GET",
-				credentials: "include",
-			}),
-			keepUnusedDataFor: DATA_LIFETIME || 60,
-			providesTags: ["Project"],
-		}),
-		getProjectEmployees: build.query<ProjectEmployeesListType, TaskPaginationType>({
-			query: ({ project_id, limit, offset, search }) => ({
-				url: `${PROJECT_EMPLOYEES_URL(project_id)}?offset=${search ? 0 : offset}&limit=${
-					search ? 0 : limit
-				}&search=${search || ""}`,
-				method: "GET",
-				credentials: "include",
-			}),
-			keepUnusedDataFor: DATA_LIFETIME || 60,
-			providesTags: ["Task", "Project"],
-		}),
-		getTasks: build.query<TaskListType, TaskPaginationType>({
-			query: ({ project_id, limit, offset, search }) => ({
-				url: `${TASKS_URL(project_id)}?offset=${search ? 0 : offset}&limit=${
-					search ? 0 : limit
-				}&search=${search || ""}`,
-				method: "GET",
-				credentials: "include",
-			}),
-			keepUnusedDataFor: DATA_LIFETIME || 60,
-			providesTags: ["Task"],
-		}),
-		getProject: build.query<ProjectType, string>({
-			query: (id) => ({
-				url: PROJECT_URL(id),
-				method: "GET",
-				credentials: "include",
-			}),
-			keepUnusedDataFor: DATA_LIFETIME || 60,
-			providesTags: ["Project"],
-		}),
-		getTask: build.query<TaskType, {project_id: string; id: string;}>({
-			query: ({ id, project_id }) => ({
-				url: TASK_URL(project_id, id),
-				method: "GET",
-				credentials: "include",
-			}),
-			keepUnusedDataFor: DATA_LIFETIME || 60,
-			providesTags: ["Task"],
-		}),
-		createProject: build.mutation<ProjectType, ProjectCreateType>({
-			query: (data) => ({
-				url: PROJECTS_URL,
-				method: "POST",
-				credentials: "include",
-				body: data,
-			}),
-			invalidatesTags: (result) => (result ? ["Project"] : []),
-		}),
-		createTask: build.mutation<
-			TaskType,
-			{project_id: string; data: TaskCreateType}
-		>({
-			query: ({ project_id, data }) => ({
-				url: TASKS_URL(project_id),
-				method: "POST",
-				credentials: "include",
-				body: data,
-			}),
-			invalidatesTags: (result) => (result ? ["Task"] : []),
-		}),
-		createProjectFile: build.mutation<
-			ProjectFileType,
-			{project_id: string; data: ProjectFileCreateType}
-		>({
-			query: ({ project_id, data }) => ({
-				url: PROJECT_FILES_URL(project_id),
-				method: "POST",
-				credentials: "include",
-				headers: {
-					"default-content-type": "use-browser-default",
-				},
-				body: generateProjectFile(data),
-			}),
-			invalidatesTags: (result) => (result ? ["Project"] : []),
-		}),
-		updateProject: build.mutation<
-			ProjectType,
-			{ id: string; data: ProjectCreateType }
-		>({
-			query: ({ id, data }) => ({
-				url: PROJECT_URL(id),
-				method: "PUT",
-				credentials: "include",
-				body: data,
-			}),
-			invalidatesTags: (result) => (result ? ["Project"] : []),
-		}),
-		updateTask: build.mutation<
-			TaskType,
-			{id: string; project_id: string; data: TaskCreateType}
-		>({
-			query: ({ id, project_id, data }) => ({
-				url: TASK_URL(project_id, id),
-				method: "PUT",
-				credentials: "include",
-				body: data,
-			}),
-			invalidatesTags: (result) => (result ? ["Task"] : []),
-		}),
-		deleteProject: build.mutation<unknown, string>({
-			query: (id) => ({
-				url: PROJECT_URL(id),
-				method: "DELETE",
-				credentials: "include",
-			}),
-			invalidatesTags: (result, error) => (!error ? ["Project"] : []),
-		}),
-		deleteTask: build.mutation<unknown, {project_id: string; id: string;}>({
-			query: ({project_id, id}) => ({
-				url: TASK_URL(project_id, id),
-				method: "DELETE",
-				credentials: "include",
-			}),
-			invalidatesTags: (result, error) => (!error ? ["Task", "Project"] : []),
-		}),
-		deleteProjectFile: build.mutation<unknown, {project_id: string; id: number}>({
-			query: ({ project_id, id }) => ({
-				url: PROJECT_FILE_URL(project_id, id),
-				method: "DELETE",
-				credentials: "include",
-			}),
-			invalidatesTags: (result, error) => (!error ? ["Project"] : []),
-		}),
-		markProjectCompleted: build.mutation<
-			{ detail: string },
-			{ id: string; action: boolean }
-		>({
-			query: ({ id, action }) => ({
-				url: PROJECT_COMPLETED_URL(id),
-				method: "POST",
-				body: { action },
-				credentials: "include",
-			}),
-			invalidatesTags: (result) => (result ? ["Project"] : []),
-		}),
-	}),
-	overrideExisting: false,
-});
-
-export const {
-	useGetProjectsQuery,
-	useGetProjectQuery,
-	useGetProjectEmployeesQuery,
-	useGetTasksQuery,
-	useGetTaskQuery,
-	useCreateProjectMutation,
-	useCreateProjectFileMutation,
-	useCreateTaskMutation,
-	useUpdateProjectMutation,
-	useUpdateTaskMutation,
-	useDeleteProjectMutation,
-	useDeleteProjectFileMutation,
-	useDeleteTaskMutation,
-	useMarkProjectCompletedMutation,
-} = projectsApi;
-
-
-/*
-import { baseApi } from "./base";
-import {
-	DATA_LIFETIME,
-	PROJECTS_URL,
-	PROJECT_URL,
-	PROJECT_COMPLETED_URL,
-	PROJECT_EMPLOYEES_URL,
-	PROJECT_FILE_URL,
-	PROJECT_FILES_URL,
-	TASKS_URL,
 	TASK_URL,
 } from "../../config";
 import {
@@ -343,7 +136,7 @@ const projectsApi = baseApi.injectEndpoints({
 				credentials: "include",
 			}),
 			keepUnusedDataFor: DATA_LIFETIME,
-			providesTags: ["Task", "Project"],
+			providesTags: [{type: "Task", id: "TASK_LIST"}, {type: "Project",id: "PROJECT_LIST"}],
 		}),
 		getProjects: build.query<ProjectListType, PaginationType>({
 			query: ({ limit, offset, search }) => ({
@@ -357,7 +150,7 @@ const projectsApi = baseApi.injectEndpoints({
 			providesTags: (data) =>
 				data
 					? [
-							{ ...data.results.map(({ id }) => ({ type: "Project", id })) },
+							...data.results.map(({ id }) => ({ type: "Project", id })),
 							{ type: "Project", id: "PROJECT_LIST" },
 					  ]
 					: [{ type: "Project", id: "PROJECT_LIST" }],
@@ -384,7 +177,7 @@ const projectsApi = baseApi.injectEndpoints({
 			providesTags: (data) =>
 				data
 					? [
-							{ ...data.results.map(({ id }) => ({ type: "Task", id })) },
+							...data.results.map(({ id }) => ({ type: "Task", id })),
 							{ type: "Task", id: "TASK_LIST" },
 					  ]
 					: [{ type: "Task", id: "TASK_LIST" }],
@@ -413,7 +206,7 @@ const projectsApi = baseApi.injectEndpoints({
 				body: data,
 			}),
 			invalidatesTags: (result, error, args) =>
-				result ? [{ type: "Project", id: args.id }, { type: "Project", id: 'LIST' }] : [],
+				result ? [{ type: "Project", id: args.id }] : [],
 		}),
 		updateTask: build.mutation<
 			TaskType,
@@ -453,5 +246,3 @@ export const {
 	useDeleteTaskMutation,
 	useMarkProjectCompletedMutation,
 } = projectsApi;
-
-*/
