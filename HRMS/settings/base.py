b397c79e3f7d25b5e428699a5a26f0ad4508e897
kingkite <kitewebdevelopers@gmail.com>
2022-06-19 15:38:56 +0100

    Removed the staticfiles storage settings

--- HRMS/settings/base.py
+++ HRMS/settings/base.py
@@ -146,7 +146,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Media files (videos)
 
